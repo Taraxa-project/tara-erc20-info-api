70c952222f4fec68a6de1aa75f35c8e35940ddd3
Elod23 <vargaelod23@gmail.com>
2023-01-11 17:22:35 +0200

    adds testnet qa delegation endpoint

--- src/config/general.ts
+++ src/config/general.ts
@@ -3,5 +3,6 @@ export default () => ({
   tokenAddress: process.env.TOKEN_ADDRESS,
   stakingAddress: process.env.STAKING_ADDRESS,
   delegationAPIRoot: process.env.DELEGATION_API_ROOT_URI,
+  testnetDelegationAPIRoot: process.env.TESTNET_DELEGATION_API_ROOT_URI,
   coinGeckoTaraxaApi: process.env.COINGECKO_API_TARAXA_URI,
 });
