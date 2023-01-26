81e7394fb0e0a9a5251300ac5cd9b9db445df773
Elod23 <vargaelod23@gmail.com>
2023-01-26 13:35:06 +0200

    initializes shared cache for all modules

--- src/github/github.module.ts
+++ src/github/github.module.ts
@@ -1,16 +1,33 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module, CacheModule } from '@nestjs/common';
+import { Module, CacheModule, CacheStore } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GitHubService } from './github.service';
 import { GitHubController } from './github.controller';
 import { GraphQLRequestModule } from '@golevelup/nestjs-graphql-request';
 import { GraphQLService } from './graphql.connector.service';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     ConfigModule,
     HttpModule,
-    CacheModule.register(),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          store: await redisStore({
+            socket: {
+              host: configService.get<string>('redisHost'),
+              port: configService.get<number>('redisPort'),
+            },
+            name: `${configService.get<string>(
+              'namePrefix',
+            )}_${configService.get<string>('redisName')}`,
+          }),
+        } as unknown as CacheStore;
+      },
+    }),
     GraphQLRequestModule.forRootAsync(GraphQLRequestModule, {
       imports: [ConfigModule],
       inject: [ConfigService],
--- src/node/node.module.ts
+++ src/node/node.module.ts
@@ -1,11 +1,32 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module, CacheModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module, CacheModule, CacheStore } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
 import { NodeController } from './node.controller';
 import { NodeService } from './node.service';
 
 @Module({
-  imports: [ConfigModule, HttpModule, CacheModule.register()],
+  imports: [
+    ConfigModule,
+    HttpModule,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          store: await redisStore({
+            socket: {
+              host: configService.get<string>('redisHost'),
+              port: configService.get<number>('redisPort'),
+            },
+            name: `${configService.get<string>(
+              'namePrefix',
+            )}_${configService.get<string>('redisName')}`,
+          }),
+        } as unknown as CacheStore;
+      },
+    }),
+  ],
   controllers: [NodeController],
   providers: [NodeService],
 })
